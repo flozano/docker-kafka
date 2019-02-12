8c6dc33f6b6be1f07782dd5c0c33b2fccd120f27
Francisco A. Lozano <flozano@gmail.com>
2019-02-12 13:47:21 +0100

    Ensure new line is added to server.properties

--- scripts/start-kafka.sh
+++ scripts/start-kafka.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 # Optional ENV variables:
-# * Every env variable starting by `_KAFKA.` will be parsed: 
+# * Every env variable starting by `_KAFKA.` will be parsed:
 #   e.g. _KAFKA.advertised.host.name=localshot will become advertised.host.name=localshot and will be written to server.properties
 
 function add_config_param {
     prop_name=$(echo "$1" | tr "_" .)
     prop_value=$2
-    echo "*** Setting '$prop_name=$prop_value'"
+	echo "*** Setting '$prop_name=$prop_value' (parsed from $1)"
     if grep -q "^$prop_name" $KAFKA_HOME/config/server.properties; then
         sed -r -i "s|($prop_name)=(.*)|\1=$prop_value|g" $KAFKA_HOME/config/server.properties
     else
@@ -18,6 +18,8 @@ function add_config_param {
 function set_server_config {
     env | grep '_KAFKA'| awk -F'=' '{print $1}' | while read var; do add_config_param ${var:7} ${!var}; done
 }
+
+echo $'\n' >>  $KAFKA_HOME/config/server.properties
 
 set_server_config
 
